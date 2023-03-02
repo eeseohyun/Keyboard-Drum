e5c5d1be42ad463bccc44ecf5433cf09ffaa90fe
eeseohyun <jinlijinyoung@gmail.com>
2023-03-02 19:44:33 +0900

    드럼사운드 객체생성

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = () => {
+	const get = (target) => {
+		return document.querySelector(target);
+	};
+
+	const getAll = (target) => {
+		return document.querySelectorAll(target);
+	};
+	const drumSounds = [
+		{ key: 81, sound: "clap.wav" },
+		{ key: 87, sound: "crash.wav" },
+		{ key: 69, sound: "hihat.wav" },
+		{ key: 65, sound: "kick.wav" },
+		{ key: 83, sound: "openhat.wav" },
+		{ key: 68, sound: "ride.wav" },
+		{ key: 90, sound: "shaker.wav" },
+		{ key: 88, sound: "snare.wav" },
+		{ key: 67, sound: "tom.wav" },
+	];
+};
+
+app();
